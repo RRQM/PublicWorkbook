736f84de1de3958b66857dbe30154885ce2e3f98
若汝棋茗 <505554090@qq.com>
2024-04-12 09:22:40 +0800

    新增demo7

---
+++ Demo007/ClientDemo/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+namespace ClientDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpClient? TcpClient { get; set; }
+
+        private Task? RefreshTask { get; set; }
+
+        private CancellationTokenSource? CancellationTokenSource { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            CancellationTokenSource = new CancellationTokenSource();
+
+            TcpClient = new TcpClient();
+            TcpClient.Setup(new TouchSocketConfig()
+                .SetRemoteIPHost("127.0.0.1:12345")
+                .SetTcpDataHandlingAdapter(() => new FixedHeaderPackageAdapter())
+                .ConfigureContainer(a =>
+                {
+                    a.AddFileLogger();
+                }).ConfigurePlugins(a =>
+                {
+                    a.UseReconnection(5, true, 1000, client =>
+                    {
+                        CancellationTokenSource.Cancel();
+                        try
+                        {
+                            RefreshTask?.Wait();
+                            CancellationTokenSource.Dispose();
+                        }
+                        catch (AggregateException)
+                        {
+                            // ignored
+                        }
+                        catch (TaskCanceledException)
+                        {
+                            // ignored
+                        }
+                        CancellationTokenSource = new CancellationTokenSource();
+                        RefreshTask = RefreshNodeServer(client, CancellationTokenSource.Token);
+                    });
+                }));
+            TcpClient.Connect();
+            RefreshTask = RefreshNodeServer(TcpClient, CancellationTokenSource.Token);
+        }
+
+        Task RefreshNodeServer(ITcpClient tcpClient, CancellationToken cts)
+        {
+            return Task.Run(async () =>
+            {
+                var text = "test";
+                while (true)
+                {
+                    if (cts.IsCancellationRequested) return;
+                    await Task.Delay(1000, cts);
+                    try
+                    {
+                        await tcpClient.SendAsync(text);
+                    }
+                    catch (Exception e)
+                    {
+                        await Console.Out.WriteLineAsync(e.Message);
+                    }
+                }
+            }, cts);
+        }
+    }
+}
---
+++ Demo007/ServerDemo/Program.cs
@@ -0,0 +1,56 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using TouchSocket.Core;
+using TouchSocket.Sockets;
+
+var service = new TcpService();
+
+service.Setup(new TouchSocketConfig()
+    .SetListenIPHosts(12345)
+    .SetTcpDataHandlingAdapter(() => new FixedHeaderPackageAdapter())
+    .SetThreadCount(200)
+    .ConfigureContainer(a =>
+    {
+        a.AddLogger(new LoggerGroup(ConsoleLogger.Default, new FileLogger()));
+    })
+    .ConfigurePlugins(a =>
+    {
+        a.UseCheckClear()
+            .SetCheckClearType(CheckClearType.All)
+            .SetTick(TimeSpan.FromSeconds(120))
+            .SetOnClose((c, t) =>
+            {
+                c.TryShutdown();
+                c.SafeClose("超时无数据");
+            });
+    }));
+
+service.Connected = async (client, _) =>
+{
+    using (var receiver = client.CreateReceiver())
+    {
+        while (true)
+        {
+            using (var receiverResult = await receiver.ReadAsync(CancellationToken.None))
+            {
+                if (receiverResult.IsClosed)
+                {
+                    return;
+                }
+                receiverResult.ByteBlock.Read(out var data, receiverResult.ByteBlock.Len);//先正常接收几次消息，然后断点打这，10秒钟左右再按F5继续，就会触发所说的BUG
+                var mes = Encoding.UTF8.GetString(data);
+                client.Logger.Info(mes);
+            }
+        }
+    }
+};
+
+service.Disconnected = (client, _) =>
+{
+    client.Logger.Info($"{client.Id}已断线");
+    return EasyTask.CompletedTask;
+};
+
+service.Start();
+
+Console.ReadLine();
