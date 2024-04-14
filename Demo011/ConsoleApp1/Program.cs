b5554f41d6200282dcca0e1605ada34efc6c30b5
若汝棋茗 <505554090@qq.com>
2024-04-14 12:57:36 +0800

    新增demo11

---
+++ Demo011/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using TouchSocket.Core;
+using TouchSocket.Http.WebSockets;
+using TouchSocket.Sockets;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new WebSocketClient();
+            client.Setup(new TouchSocketConfig()
+                .SetRemoteIPHost("ws://127.0.0.1:8888")
+                .ConfigureContainer(a =>
+                {
+                    a.AddConsoleLogger();
+                }));
+            client.Connect();
+            client.Logger.Info("连接成功");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Demo011/WebSocketTest/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Fleck;
+using System;
+using System.Collections.Generic;
+using System.Net.WebSockets;
+using System.Windows;
+using System.Windows.Threading;
+
+namespace WebSocketTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebSocketServer server = new WebSocketServer("ws://127.0.0.1:8888");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            
+
+        
+            FleckLog.Level = LogLevel.Debug;
+            var allSockets = new List<IWebSocketConnection>();
+          
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    list.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        list.Items.Add("Close");
+                    }));
+                    socket.Send("客户端关闭连接"); 
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                   socket.Send("服务端发送: " + message); 
+                //Console.WriteLine(message);
+                list.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        list.Items.Add(message);
+                    }));
+                //        allSockets.ToList().ForEach(s => s.Send("Echo: " + message));
+            };
+            });
+
+        }
+
+      
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            
+            server.Dispose();
+            
+
+        }
+
+      
+    }
+
+}
