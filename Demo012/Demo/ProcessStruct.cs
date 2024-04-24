f38f66380d39319aabca9c0faf47ddb7d6c3b1a1
若汝棋茗 <505554090@qq.com>
2024-04-24 22:46:35 +0800

    添加示例demo

---
+++ Demo012/Demo/ProcessStruct.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchSocket.Core;
+
+namespace Demo
+{
+    public class ProcessStruct
+    {
+        public Bitmap Icon { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+
+    public class BitmapFastBinaryConverter : FastBinaryConverter<Bitmap>
+    {
+        protected override Bitmap Read(byte[] buffer, int offset, int len)
+        {
+            var byteBlock = new ValueByteBlock(buffer);
+            byteBlock.Pos = offset;
+            byte[] byteArray = byteBlock.ReadBytesPackage();
+            using (var stream = new MemoryStream(byteArray))
+            {
+                return new Bitmap(stream);
+            }
+        }
+
+        protected override int Write(ByteBlock byteBlock, Bitmap obj)
+        {
+            var pos = byteBlock.Pos;
+            using (var stream = new MemoryStream())
+            {
+                obj.Save(stream, ImageFormat.Png);
+                byte[] byteArray = stream.ToArray();
+
+                byteBlock.WriteBytesPackage(byteArray);
+            }
+            return byteBlock.Pos - pos;
+        }
+    }
+}
---
+++ Demo012/Demo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Demo.Properties;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchSocket.Core;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //添加转换器
+            FastBinaryFormatter.AddFastBinaryConverter(typeof(Bitmap),new BitmapFastBinaryConverter());
+
+            var process = new ProcessStruct
+            {
+                Icon = Resources.program,
+                Name = "Program",
+                Path = "/program"
+            };
+            byte[] data = SerializeConvert.FastBinarySerialize(process);
+            var de_data = SerializeConvert.FastBinaryDeserialize<ProcessStruct>(data);
+            Console.WriteLine(de_data.Name);
+        }
+    }
+}
