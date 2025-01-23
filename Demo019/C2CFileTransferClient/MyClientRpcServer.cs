dc2eb16692ff186f916fb9f7f9edc514d5a2746d
790978 <790978@qq.com>
2025-01-23 16:40:29 +0800

    Client to Client FileTransfer

---
+++ Demo019/C2CFileTransferClient/MyClientRpcServer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TouchSocket.Core;
+using TouchSocket.Dmtp.Rpc;
+using TouchSocket.Rpc;
+
+namespace C2CFileTransferClient
+{
+    internal partial class MyClientRpcServer : RpcServer
+    {
+        private readonly ILog m_logger;
+
+        public MyClientRpcServer(ILog logger)
+        {
+            this.m_logger = logger;
+        }
+
+        [DmtpRpc(MethodInvoke = true)]//使用函数名直接调用
+        public bool Notice(string msg)
+        {
+            this.m_logger.Info(msg);
+            return true;
+        }
+    }
+}
