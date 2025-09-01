0b3935f51f450b7a4e3211add4d3ea8e0d3be7be
标标 <ptkatz2@ydsec.com>
2025-09-01 11:05:00 +0800

    Demo023

---
+++ Demo023/Demo23/Program.cs
@@ -0,0 +1,28 @@
+﻿using TouchSocket.Core;
+using TouchSocket.JsonRpc;
+using TouchSocket.Rpc;
+using TouchSocket.Sockets;
+
+namespace Demo23
+{
+    internal class Program
+    {
+        private static ManualResetEvent _manualReset = new(false);
+        static async Task Main(string[] args)
+        {
+            var client = new WebSocketJsonRpcClient();
+            await client.SetupAsync(new TouchSocketConfig()
+                .SetRemoteIPHost("ws://127.0.0.1:9010/ws")
+                .ConfigurePlugins(a =>
+                {
+                    a.UseWebSocketHeartbeat()
+                    .SetTick(TimeSpan.FromSeconds(5));
+                    a.UseWebSocketReconnection()
+                    .UsePolling(TimeSpan.FromSeconds(5));
+                }));
+            var result = await client.TryConnectAsync();
+            Console.WriteLine(result);
+            _manualReset.WaitOne();
+        }
+    }
+}
---
+++ Demo023/Server/Program.cs
@@ -0,0 +1,33 @@
+﻿using TouchSocket.Core;
+using TouchSocket.Http;
+using TouchSocket.Rpc;
+using TouchSocket.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        private static ManualResetEvent _manualReset = new(false);
+        static async Task Main(string[] args)
+        {
+            var service = new HttpService();
+
+            await service.SetupAsync(new TouchSocketConfig()
+                 .SetListenIPHosts(9010)
+                 .ConfigurePlugins(a =>
+                 {
+                     a.UseWebSocket()
+                     .SetWSUrl("/ws");
+                     a.UseWebSocketJsonRpc()
+                     .SetAllowJsonRpc((SessionClient, context) =>
+                     {
+                         return true;
+                     });
+                     a.Add<MyPluginClass>();
+                 }));
+
+            await service.StartAsync();
+            _manualReset.WaitOne();
+        }
+    }
+}
